38f604d64636c2e9c6bfefd8a445b8c99eb60a40
Acer10 <Acer10@137.125.152.10>
2023-01-25 13:13:47 -0500

    3rd

--- src/main/java/com/mycompany/helloworld/HelloWorld.java
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -9,6 +9,6 @@ package com.mycompany.helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Worldian");
+        System.out.println("BCS Third Commit");
     }
 }
